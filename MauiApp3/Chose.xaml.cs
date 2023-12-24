28710f6ac99ed496e6847bafa1fda1a2c7082c4a
user <user@DESKTOP-SRDNU3H>
2023-12-24 21:58:21 +0800

    加入專案檔案。

---
+++ MauiApp3/Chose.xaml.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+namespace MauiApp3
+{
+    public partial class Chose : ContentPage
+    {
+        public static List<string> textList { get; private set; } = new List<string>();
+        public Chose()
+        {
+            InitializeComponent();
+            textList = new List<string>();
+
+        }
+        private void OnShowTextClicked(object sender, EventArgs e)
+        {
+            // 获取输入框中的文本
+            string userInput = inputEntry.Text;
+
+            // 将文本添加到列表
+            textList.Insert(0, userInput);
+
+            // 保留最近的九个文本
+            textList = textList.Take(9).ToList();
+
+            UpdateFlexLayout();
+            
+
+        }
+        private void UpdateFlexLayout()
+        {
+            // 清除现有内容
+            outputFlexLayout.Children.Clear();
+
+            // 添加文本和按钮到 FlexLayout
+            foreach (var text in textList)
+            {
+                var label = new Label { Text = text };
+                var button = new Button { Text = "移除", CommandParameter = text };
+                button.Clicked += OnButtonClicked;
+
+                outputFlexLayout.Children.Add(label);
+                outputFlexLayout.Children.Add(button);
+            }
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            // 处理按钮点击事件
+            var buttonText = (sender as Button).CommandParameter as string;
+     
+            // 從列表中移除相應的文字
+            textList.Remove(buttonText);
+
+           
+
+            // 更新 FlexLayout 顯示的文本和按鈕
+            UpdateFlexLayout();
+            
+        }
+        private async void OnClicked(object sender, EventArgs e)
+        {
+            if (textList.Count>1)
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+            if (textList.Count <= 1)
+            {
+                await DisplayAlert("提醒", "請加入至少兩個選擇", "確定");
+            } 
+        }
+    }
+}
+
---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> textListFromChose = Chose.textList;
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializePage();
+        }
+        private void InitializePage()
+        {
+   
+            // 遍历 textListFromChose，将值分配给相应的 Label
+            for (int i = 0; i < textListFromChose.Count && i < 9; i++)
+            {
+                string labelText = textListFromChose[i];
+
+                Label label = FindLabel($"label{i + 1}"); // Assuming your Label names are like "label1", "label2", etc.
+
+                // 分配值给 Label
+                label.Text = labelText;
+            }
+        }
+        // 辅助方法，用于根据名称查找 Label 控件
+        private Label FindLabel(string name)
+        {
+            return this.FindByName<Label>(name);
+        }
+        private Frame FindFrame(string name)
+        {
+            return this.FindByName<Frame>(name);
+        }
+        private async void OnStartClicked(object sender, EventArgs e)
+        {
+            for (int i = 0; i < textListFromChose.Count; i++)
+            {
+                Frame frame = FindFrame($"Frame{i + 1}");  // Assuming your Label names are like "frame1", "frame2", etc.
+                                                           // 改变 Frame 背景颜色
+                frame.BackgroundColor = Color.FromHex("#FFFFFF"); // 你可以替换为你想要的颜色
+            }
+            System.Random random = new System.Random();
+            int randomIncrement = random.Next(1, textListFromChose.Count+1);
+            for (int j = 0; j < 3; j++)
+            {
+                if(j!=2)
+                {
+                    for (int i = 0; i < textListFromChose.Count; i++)
+                    {
+                        Frame frame = FindFrame($"Frame{i + 1}");  // Assuming your Label names are like "frame1", "frame2", etc.
+                                                                   // 改变 Frame 背景颜色
+                        frame.BackgroundColor = Color.FromHex("#FFFF00");  // 你可以替换为你想要的颜色
+                                                                           // 在0.5秒后还原颜色
+                        await Task.Delay(100);
+                        Device.BeginInvokeOnMainThread(() =>
+                        {
+                            frame.BackgroundColor = Color.FromHex("#FFFFFF"); // 还原为原始颜色
+                        });
+                    }
+                }
+                if (j == 2)
+                {
+                    for (int i = 0; i < randomIncrement; i++)
+                    {
+                        if (i != randomIncrement - 1)
+                        {
+                            Frame frame = FindFrame($"Frame{i + 1}");  // Assuming your Label names are like "frame1", "frame2", etc.
+                                                                       // 改变 Frame 背景颜色
+                            frame.BackgroundColor = Color.FromHex("#FFFF00");  // 你可以替换为你想要的颜色
+                                                                               // 在0.5秒后还原颜色
+                            await Task.Delay(100);
+                            Device.BeginInvokeOnMainThread(() =>
+                            {
+                                frame.BackgroundColor = Color.FromHex("#FFFFFF"); // 还原为原始颜色
+                            });
+                        }
+                        if (i == randomIncrement - 1)
+                        {
+                            Frame frame = FindFrame($"Frame{i + 1}");  // Assuming your Label names are like "frame1", "frame2", etc.
+                                                                       // 改变 Frame 背景颜色
+                            frame.BackgroundColor = Color.FromHex("#FFFF00");  // 你可以替换为你想要的颜色
+                                                                               // 在0.5秒后还原颜色
+                        }
+                      
+                    }
+                }
+            }
+          
+        }
+    }
+
+}
